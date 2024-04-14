08537f6c54b43ba065ceef623dcfc58180d3aeb7
wolferlk <sasiofficial25@gmail.com>
2024-04-14 15:11:17 +0530

    class show
    class edit

---
+++ Royal Institite/backend/models/Classes.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const classSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  teacher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Teacher' // Make sure you have a 'Teacher' model defined
+  },
+  subject: {
+    type: String,
+    required: true
+  },
+  day: {
+    type: String,
+    required: true
+  },
+  time: {
+    type: String,
+    required: true
+  },
+  grade: {
+    type: Number,
+    required: true
+  }
+});
+
+export const Classes = mongoose.model("Classes", classSchema);
+
